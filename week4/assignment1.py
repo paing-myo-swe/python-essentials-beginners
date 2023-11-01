3764d0b73aec67be9a4fb5d49d8c469d9b606263
paing-myo-swe <paingmyoswe.dev@gmail.com>
2023-11-01 19:50:05 +0700

    numbers part 1

---
+++ week4/assignment1.py
@@ -0,0 +1,9 @@
+# Program: Find GCD of two numbers
+import math
+
+print("Greatest Common Divisor finder of two numbers.....")
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+print("GCD of",num1,"and",num2,"is:",math.gcd(num1,num2))
---
+++ week4/math/abs.py
@@ -0,0 +1,9 @@
+import math
+
+a = -75
+
+b = abs(a)
+print("Absolute value of",a,"by abs() is",b)
+
+c = math.fabs(a)
+print("Absolute value of",a,'by fabs() is',c)
